22587efc91cc2f6399e76c86c221209f2d1b7404
isha288 <kaushalisha114@gmail.com>
2023-06-13 10:21:59 +0200

    edited stylingSheet, index.html, LoginView

--- src/main/java/com/blackjack/database/LoginView.java
+++ src/main/java/com/blackjack/database/LoginView.java
@@ -1,6 +1,7 @@
 package com.blackjack.database;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.formlayout.FormLayout;
@@ -9,6 +10,7 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -21,29 +23,45 @@ public class LoginView extends VerticalLayout {
     public LoginView() {
         setJustifyContentMode(JustifyContentMode.CENTER);
         setAlignItems(Alignment.CENTER);
-        Image image = new Image("download.jpg", "Logo");
-        
-        
+
         H2 heading = new H2("Login");
         heading.getStyle().set("font-size", "24px");
-        
+        heading.setClassName("h2Login");
+
+
         TextField usernameField = new TextField("Username");
+        //usernameField.getStyle().set("background-color", "#ffffff");
         usernameField.setWidth("300px");
+        usernameField.addThemeVariants(
+                TextFieldVariant.LUMO_SMALL,
+                TextFieldVariant.LUMO_ALIGN_RIGHT,
+                TextFieldVariant.LUMO_HELPER_ABOVE_FIELD
+        );
+        usernameField.getStyle().set("--vaadin-input-field-border-width", "1px");
+
 
         PasswordField passwordField = new PasswordField("Password");
         passwordField.setWidth("300px");
+        passwordField.addThemeVariants(
+                TextFieldVariant.LUMO_SMALL,
+                TextFieldVariant.LUMO_ALIGN_RIGHT,
+                TextFieldVariant.LUMO_HELPER_ABOVE_FIELD
+        );
+        passwordField.getStyle().set("--vaadin-input-field-border-width", "1px");
 
         Button loginButton = new Button("Login");
         loginButton.setWidth("100px");
+        loginButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
         loginButton.addClickListener(event -> {
             String username = usernameField.getValue();
             String password = passwordField.getValue();
 
             try {
-                if (authenticate(username, password)) {
-                    Notification.show("Login successful");
-                } else {
+                if (!authenticate(username, password)) {
                     Notification.show("Invalid credentials");
+                } else {
+                    Notification.show("Login successful");
                 }
             } catch (SQLException e) {
                 throw new RuntimeException(e);
@@ -54,11 +72,12 @@ public class LoginView extends VerticalLayout {
 
         Button registerButton = new Button("Register");
         registerButton.setWidth("100px");
+        registerButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         registerButton.addClickListener(event -> {
             showRegistrationForm();
         });
 
-        add(image, heading, usernameField, passwordField, loginButton, registerButton);
+        add(heading, usernameField, passwordField, loginButton, registerButton);
     }
 
     private boolean authenticate(String username, String password) throws SQLException, ClassNotFoundException {
